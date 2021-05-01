8355865790c7b600ad2193e187f1548127810c4c
ravish523 <ravishkr87@gmail.com>
2021-05-01 10:35:58 +0530

    Update in testCase Tc_005 finally completed

--- src/test/java/com/Eccomerce/PageObject/MyWishlistpage.java
+++ src/test/java/com/Eccomerce/PageObject/MyWishlistpage.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class MyWishlistpage {
+import com.Eccomerce.TestCase.BaseClass;
+
+import junit.framework.Assert;
+
+public class MyWishlistpage extends BaseClass{
 	
 public WebDriver driver;
 	
@@ -17,6 +21,8 @@ public WebDriver driver;
 	private WebElement txtmessageshare;
 	@FindBy(xpath="//span[contains(text(),'Share Wishlist')]")
 	private WebElement txtshartoemail;
+	@FindBy(xpath="//span[contains(text(),'Your Wishlist has been shared.')]")
+	private WebElement txtshareWishListcnf;
 	
 	 public MyWishlistpage(WebDriver d)
 	 {
@@ -36,6 +42,28 @@ public WebDriver driver;
 	 public void ClickOnShareButton()
 	 {
 		 txtshartoemail.click();
+	 }
+	 public void verifycnfirmationShareWishList()
+	 {
+		 try
+		 {
+			 if(txtshareWishListcnf.getText().contains("Your Wishlist has been shared."))
+			 {
+				 logger.info("Wishlist has been shared sucessfully....Pass");
+				 Assert.assertTrue(true);
+			 }
+			 else
+			 {
+				 logger.info("Wishlist has not been shared sucessfully....Pass");
+				 CaptureScreen(driver,"VerifyCreateAccountWishlist");
+				 Assert.assertTrue(false);
+			 }
+			 
+		 }
+		 catch(Exception e)
+		 {
+			 System.out.println("Exception message :"+e.getMessage());
+		 }
 	 }
 
 }
--- src/test/java/com/Eccomerce/TestCase/TC_005.java
+++ src/test/java/com/Eccomerce/TestCase/TC_005.java
@@ -2,6 +2,7 @@ package com.Eccomerce.TestCase;
 
 import java.io.IOException;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.Test;
 
 import com.Eccomerce.PageObject.AccountMainHomePage;
@@ -16,13 +17,14 @@ public class TC_005 extends BaseClass{
 	@Test
 	public void VerifyCreateAccountWishlist() throws InterruptedException, IOException
 	{
+		String add=RandomStringUtils.randomAlphabetic(5);
 		MagnetoHomePage mhp=new MagnetoHomePage(driver);
 		mhp.ClickOnMyAccount();
 		CreateAccountAndLoginPage calp=new CreateAccountAndLoginPage(driver);
 		calp.ClickOnCreateAccount();
 		Thread.sleep(2000);
 		CreatingAccountFormPage cafp=new CreatingAccountFormPage(driver);
-		cafp.EnterDetailsInCreatingAccountForm("radvi", "kumar", "ranjdan", "ravi@23gmail.com", "kumar@1234", "kumar@1234");
+		cafp.EnterDetailsInCreatingAccountForm(add, "kumar", "ranjdan", add+"@23gmail.com", "kumar@1234", "kumar@1234");
 	    cafp.ClickOnRegister();
 	    Thread.sleep(2000);
 	    AccountMainHomePage amhp=new AccountMainHomePage(driver);
@@ -36,7 +38,7 @@ public class TC_005 extends BaseClass{
 	    mwlp.ClickOnShareWishList();
 	    mwlp.EnterDetailsToShare("ravishkr54@gmail.com", "demo");
 	    mwlp.ClickOnShareButton();
-	    
+	    mwlp.verifycnfirmationShareWishList();
 	    
 	    
 	    

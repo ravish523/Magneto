a334d07e48f91e845696156f40b35a4e84e1152e
ravish523 <ravishkr87@gmail.com>
2021-06-06 01:03:51 +0530

    Adding the Xutils class and test data folder

--- src/test/java/com/Eccomerce/TestCase/demoInterview.java
+++ src/test/java/com/Eccomerce/TestCase/demoInterview.java
@@ -1,16 +1,58 @@
 package com.Eccomerce.TestCase;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.testng.annotations.Test;
 
-public class demoInterview {
-	@Test
-	public void temoTest()
-	{
-	System.setProperty("webdriver.chrome.driver","C:\\work1\\chromedriver.exe");
-	WebDriver driver=new ChromeDriver();
-	driver.get("");
-	}
+import com.Eccomerce.Utilities.Xutil;
 
+public class demoInterview{
+	
+	@Test
+	public void DemoTest() throws IOException, EncryptedDocumentException, InvalidFormatException
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=2;j++)
+			{
+
+				String value=Xutil.getCellData("./TestData/TestCase.xlsx", "login", i, j);
+				System.out.print(" -1- "+value+" -1- ");
+				Xutil.setCellData("./TestData/TestCase.xlsx", "login", i, 3, "pass");
+				System.out.println(" fffffffffffffff............");
+				String value2=Xutil.getCellData("./TestData/TestCase.xlsx", "login", i, 3);
+				System.out.print(" -2- "+value2+" -2- ");
+			}
+			System.out.println(" ");
+			
+		}
+		
+		
+	   
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	 
 }
